df6ca842896f6ed7cc03604e4f9587433ed0a477
Ralph Castain <rhc@pmix.org>
2021-07-26 09:19:28 -0700

    Few minor changes and add CentOS8 support
    Add a few packages to CentOS7 build to extend PMIx support
    and aid in debugging. Add Dockerfile for CentOS8. Update the
    Ubuntu Dockerfile to track changes in the Docker library.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 docker build -t ompi-toy-box:latest -f Dockerfile.ssh .
 
-#docker build -t ompi-toy-box:ubi8 -f Dockerfile.ssh.ubi8 .
+docker build -t ompi-toy-box:cnt8 -f Dockerfile.ssh.cnt8 .
+
+docker build -t ompi-toy-box:ubi8 -f Dockerfile.ssh.ubi8 .
