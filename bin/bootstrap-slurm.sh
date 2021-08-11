096ce8d6fbddd763542eae3067edaee6a387597b
Isaías A. Comprés Ureña <compresu@in.tum.de>
2021-08-11 17:04:56 +0200

    Base Slurm instalation works.

--- bin/bootstrap-slurm.sh
+++ bin/bootstrap-slurm.sh
@@ -4,17 +4,22 @@ MUNGE_INSTALL_DIR=/opt/hpc/local/munge
 SLURM_INSTALL_DIR=/opt/hpc/external/slurm
 
 while read h; do
-	ssh $h "${MUNGE_INSTALL_DIR}/sbin/munged -Ff > munge_remote_daemon_out 2>&1" &
+	echo "starting munged in host $h"
+	#ssh $h "${MUNGE_INSTALL_DIR}/sbin/munged -Ff > munge_remote_daemon_out 2>&1" &
+	ssh $h "${MUNGE_INSTALL_DIR}/sbin/munged"
 done </opt/hpc/external/slurm/etc/unique_hosts
 
 # make sure the munged daemons have started before we begin starting the slurmds
 sleep 2
 
 while read h; do
-	ssh $h "${SLURM_INSTALL_DIR}/sbin/slurmd -Dc > slurm_remote_daemon_out 2>&1" &
+	echo "starting slurmd in host $h"
+	#ssh $h "${SLURM_INSTALL_DIR}/sbin/slurmd -Dc > slurm_remote_daemon_out 2>&1" &
+	ssh $h "${SLURM_INSTALL_DIR}/sbin/slurmd" &
 done </opt/hpc/external/slurm/etc/unique_hosts
 
 echo "starting the controller..."
-${SLURM_INSTALL_DIR}/sbin/slurmctld -Dcvvvv > slurm_controller_out 2>&1 &
+#${SLURM_INSTALL_DIR}/sbin/slurmctld -Dcvvvv > slurm_controller_out 2>&1 &
+${SLURM_INSTALL_DIR}/sbin/slurmctld
 sleep 5
 
--- bin/generate-slurm-conf.sh
+++ bin/generate-slurm-conf.sh
@@ -17,13 +17,18 @@ echo "copying initial slurm.conf work file"
 cp -a ./bin/slurm-input/slurm.conf.in ./tmp/slurm.conf.initial
 cp -a ./bin/slurm-input/slurm.conf.in ./tmp/slurm.conf.work
 echo "setting up NodeName and PartitionName entries in slurm.conf ..."
+rm ./tmp/hosts
 HostNumber=1
 while read h; do
+	#echo "NodeName=$h NodeAddr=$h CPUs=${CORES_PER_NODE} State=UNKNOWN" >> ./tmp/slurm.conf.work
+	#echo "NodeName=$h NodeAddr=$h CPUs=${CORES_PER_NODE} State=UNKNOWN" >> ./tmp/slurm.conf.work
 	echo "NodeName=${USER}-node0${HostNumber} NodeAddr=$h CPUs=${CORES_PER_NODE} State=UNKNOWN" >> ./tmp/slurm.conf.work
+	echo "$h	${USER}-node0${HostNumber}" >> ./tmp/hosts
+	echo "${USER}-node0${HostNumber}" >> ./tmp/nodes
 	((HostNumber++))
 done <./tmp/unique_hosts
-Nodes=`sed "N;s/\n/,/" ./tmp/unique_hosts`
-Nodes=`cat ./tmp/unique_hosts | paste -sd "," -`
+#Nodes=`sed "N;s/\n/,/" ./tmp/nodes`
+Nodes=`cat ./tmp/nodes | paste -sd "," -`
 echo "PartitionName=local Nodes=${Nodes} Default=YES MaxTime=INFINITE State=UP" >> ./tmp/slurm.conf.work
 
 FirstNodeName="${USER}-node01"
@@ -33,5 +38,6 @@ echo "ControlAddr=${FirstNode}" >> ./tmp/slurm.conf.work
 mkdir -p ./install/slurm/etc/
 cp -a ./tmp/slurm.conf.work ./install/slurm/etc/slurm.conf
 cp -a ./tmp/unique_hosts ./install/slurm/etc/unique_hosts
+cp -a ./tmp/hosts ./install/slurm/etc/hosts
 
 exit 0
