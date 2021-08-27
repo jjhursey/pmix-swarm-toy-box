41b858b448cab9198d4f751be327249f318f54ea
Isaías Comprés <isaias.compres@tum.de>
2021-08-27 18:43:18 +0200

    Fixed issue related to Open MPI picking second net and hanging.

--- bin/build-openmpi.sh
+++ bin/build-openmpi.sh
@@ -5,6 +5,7 @@
             --with-hwloc=${HWLOC_INSTALL_PATH} \
             --with-libevent=${LIBEVENT_INSTALL_PATH} \
             --with-pmix=${PMIX_ROOT} \
+            --with-slurm=yes \
             --enable-mpirun-prefix-by-default \
             2>&1 | tee configure.log.$$ 2>&1
 make -j 10 2>&1 | tee make.log.$$ 2>&1
